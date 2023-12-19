f3f7e66308473fb5e14f8a0e9fa5b015a44c07b4
Wesley-Alan <136496136+Wesley-Alan@users.noreply.github.com>
2023-12-19 08:01:40 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//The unordered list where the player’s guessed letters will appear.
+const guessedLettersList = document.querySelector(".guessed-letters");
+//The button with the text “Guess!” in it.
+const guessButton = document.querySelector(".guess");
+//The text input where the player will guess a letter.
+const letterInput = document.querySelector(".letter");
+//The empty paragraph where the word in progress will appear.
+const wordInProgress = document.querySelector(".word-in-progress");
+//The paragraph where the remaining guesses will display.
+const remainingGuesses = document.querySelector(".remaining");
+//The span inside the paragraph where the remaining guesses will display.
+const remainingGuessesSpan = document.querySelector(".remaining span");
+//The empty paragraph where messages will appear when the player guesses a letter.
+const message = document.querySelector(".message");
+//The hidden button that will appear prompting the player to play again.
+const playAgainButton = document.querySelector(".play-again");
+
+
+const word = "magnolia";
+
+const placeholder = function () {
+    const placeholderLetters = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderLetters.push("●");
+    }
+    wordInProgress.innerText = placeholderLetters.join("");
+};
+
+placeholder(word);
+
+guessButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const show = letterInput.value;
+    console.log(show);
+    letterInput.value = "";
+
+
+} );
